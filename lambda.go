22f9347531859b810b3f5428903822f4fb2306b0
munkh-altai <munkaltai@gmail.com>
2023-04-14 12:36:22 +0800

    table proto to lambda start

--- lambda.go
+++ lambda.go
@@ -2,10 +2,13 @@ package lambda
 
 import (
 	"embed"
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html"
 	"github.com/lambda-platform/lambda/config"
+	"github.com/lambda-platform/lambda/generator"
 	"github.com/lambda-platform/lambda/puzzle/views"
+	"os"
 )
 
 var viewsfs embed.FS
@@ -16,7 +19,35 @@ type Lambda struct {
 }
 
 func (lambda *Lambda) Start() {
-	lambda.App.Listen(":" + config.Config.App.Port)
+	if len(os.Args) < 2 {
+
+		lambda.App.Listen(":" + config.Config.App.Port)
+
+	} else {
+		command := os.Args[1]
+
+		switch command {
+
+		case "table":
+			if len(os.Args) < 3 {
+				fmt.Println("Please provide table name: table your-table-name")
+			} else {
+				table := os.Args[2]
+				generator.GetStruct(table)
+			}
+		case "proto":
+			if len(os.Args) < 3 {
+				fmt.Println("Please provide table name: table your-table-name")
+			} else {
+				table := os.Args[2]
+				generator.GetProtobuf(table)
+			}
+		default:
+			fmt.Printf("Unknown command: %s\n", command)
+			os.Exit(1)
+		}
+	}
+
 	//defer DB.DB.Close()
 }
 
