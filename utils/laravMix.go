eeec35b2e49818b067742b5d1a99037455aa24a8
munkh-altai <munkaltai@gmail.com>
2023-04-28 15:41:43 +0800

    mixload removed

--- utils/laravMix.go
+++ utils/laravMix.go
@@ -1,9 +1,6 @@
 package utils
 
 import (
-	"encoding/json"
-	"fmt"
-	"os"
 	"sync"
 )
 
@@ -11,31 +8,31 @@ var onceMix sync.Once
 
 var Mix map[string]string
 
-func init() {
-
-	onceMix.Do(func() {
-
-		mixFile := "public/mix-manifest.json"
-
-		//if runtime.GOOS == "windows" {
-		//	dir, _ := os.Getwd()
-		//
-		//	fmt.Println("dir windows:")
-		//	fmt.Println(dir)
-		//
-		//	mixFile = dir + "/public/mix-manifest.json"
-		//	fmt.Println(mixFile)
-		//}
-
-		data, err := os.ReadFile(mixFile)
-
-		if err != nil {
-			fmt.Println("MIX FILE NOT FOUND")
-		}
-		err2 := json.Unmarshal(data, &Mix)
-		if err2 != nil {
-			fmt.Println("File parse error")
-		}
-	})
-
-}
+//func init() {
+//
+//	onceMix.Do(func() {
+//
+//		mixFile := "public/mix-manifest.json"
+//
+//		//if runtime.GOOS == "windows" {
+//		//	dir, _ := os.Getwd()
+//		//
+//		//	fmt.Println("dir windows:")
+//		//	fmt.Println(dir)
+//		//
+//		//	mixFile = dir + "/public/mix-manifest.json"
+//		//	fmt.Println(mixFile)
+//		//}
+//
+//		data, err := os.ReadFile(mixFile)
+//
+//		if err != nil {
+//			fmt.Println("MIX FILE NOT FOUND")
+//		}
+//		err2 := json.Unmarshal(data, &Mix)
+//		if err2 != nil {
+//			fmt.Println("File parse error")
+//		}
+//	})
+//
+//}
