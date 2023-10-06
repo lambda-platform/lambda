3f65009f7697b62e348f9a5644eb047afa07667d
munkh-altai <munkaltai@gmail.com>
2023-10-06 09:21:17 +0800

    user token's id type to int64

--- agent/handlers/authHandlers.go
+++ agent/handlers/authHandlers.go
@@ -240,7 +240,14 @@ func CreateJwtToken(user map[string]interface{}, role int64) (string, error) {
 
 	for k, v := range user {
 		if k != "role" && k != "password" {
-			claims[k] = v
+			if (!config.Config.SysAdmin.UUID && k == "id") || (!config.Config.SysAdmin.UUID && k == "ID") {
+
+				claims[k] = agentUtils.GetRole(v)
+
+			} else {
+				claims[k] = v
+			}
+
 		}
 	}
 	//for i := 0; i < len(config.LambdaConfig.UserDataFields); i++ {
