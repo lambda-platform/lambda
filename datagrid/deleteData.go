5e8a96413dc2045d63faa1d49b9a081b5f6bbb06
munkh-altai <munkaltai@gmail.com>
2024-03-20 10:14:58 +0800

    fixed bug

--- datagrid/deleteData.go
+++ datagrid/deleteData.go
@@ -9,7 +9,6 @@ import (
 func DeleteData(c *fiber.Ctx, datagrid Datagrid, id string) error {
 	qr := DB.DB.Where(datagrid.Identity+" = ?", id)
 	ExecTrigger("beforeDelete", id, datagrid, qr, c)
-	//fmt.Println(Identity, id, "Identity, id")
 
 	err := qr.Delete(datagrid.MainModel).Error
 
