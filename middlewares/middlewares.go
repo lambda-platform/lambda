17cc271c232d3cbccd29689c3663faffb84166fb
munkh-altai <munkaltai@gmail.com>
2025-02-26 14:09:52 +0800

    rate limit temprary disabled

--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -2,16 +2,14 @@ package middlewares
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/limiter"
-	"time"
 )
 
 func Set(app *fiber.App) {
 	// Rate limiter middleware
-	app.Use(limiter.New(limiter.Config{
-		Expiration: 30 * time.Second, //
-		Max:        1000,             //
-	}))
+	//app.Use(limiter.New(limiter.Config{
+	//	Expiration: 30 * time.Second, //
+	//	Max:        1000,             //
+	//}))
 
 	/*
 		|----------------------------------------------
